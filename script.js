2dad50f8c22d38b7237558892b3f60aaa3f54aa2
shivdasbhande05 <shivdasbhande@gmail.com>
2025-02-04 22:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+const getName = document.querySelector("#username");
+const getPassword = document.querySelector("#password");
+const checkbox = document.querySelector("#checkbox");
+const submitBtn = document.querySelector("#submit");
+const existingBtn = document.querySelector("#existing");
+
+const savedName = localStorage.getItem("Username");
+const savedPassword = localStorage.getItem("Password");
+
+if(savedName && savedPassword){
+    existingBtn.style.display = "block";
+} else {
+    existingBtn.style.display = "none";
+}
+
+
+function setItem() {
+    localStorage.setItem("Username",getName.value.trim());
+    localStorage.setItem("Password",getPassword.value.trim());
+}
+function removeItem() {
+    localStorage.removeItem("Username",getName.value.trim());
+    localStorage.removeItem("Password",getPassword.value.trim());
+}
+
+submitBtn.addEventListener("click" , (e) => {
+    e.preventDefault();
+    if(checkbox.checked){
+        setItem();
+    } else {
+        removeItem();
+    }
+
+    alert(`Logged in as ${getName.value.trim()}`);
+
+    existingBtn.style.display = "block";
+});
+
+existingBtn.addEventListener("click",() => {
+    if(savedName){
+        alert(`Logged in as ${savedName}`);
+    }
+})
+
+})
+
+
